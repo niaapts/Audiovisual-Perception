f7416479ba8c7a7143ebcd55012162a78d2e1cb1
Nia Aptsiauri <aptsiauri.niaa@gmail.com>
2024-05-22 12:24:02 +0200

    final version of my code, with small corrections

--- audiovisual_exp.py
+++ audiovisual_exp.py
@@ -5,49 +5,35 @@
 import random
 from expyriment import design, control, stimuli, io, misc
 
-N_TRIALS = 15
+#All the needed variables
 MAX_FLASH=4
 MAX_BEEPS = 4 # Variable number of auditory beeps
-FLASH_DURATION= 17
+WAIT_AFTER_FIRST_BEEP=16
 FLASH_INTERVAL=55
 MAX_RESPONSE_DELAY = 2000
-
+WHITE=(255,255,255)
 
 #Calculate the screen coordinates for 5 degrees eccentricity, I used the numbers that ChatGPT provided to me
 eccentricity_degrees = 5
 pixels_per_degree = 28
 viewing_distance_cm = 57
 
-#5 degrees at horizontal eccentricity
+#horizontal eccentricity at 5 degrees
 eccentricity_pixels = eccentricity_degrees * pixels_per_degree
 eccentricity_screen_coordinates = (eccentricity_pixels, 0)  
 
+#experimental design
 exp = design.Experiment(name="Audiovisual", text_size=40)
 control.set_develop_mode(on=True)
-#expyriment.control.default.window_mode=True
 
 control.initialize(exp)
 
+#audio stimuli with 1-4 beeps
 beeps= {
     1: stimuli.Audio('beep-1.wav'),
     2: stimuli.Audio('beep-2.wav'),
     3: stimuli.Audio('beep-3.wav'),
     4: stimuli.Audio('beep-4.wav')}
-
-
-visual_stimulus = stimuli.Circle(radius=50, position=eccentricity_screen_coordinates, colour=(255, 255, 255))  #stimuli positied according to the eccentricity
-blankscreen = stimuli.BlankScreen()
-
-#Instructions for the participants, variables for data file
-instructions = stimuli.TextScreen("Instructions",
-    f"""You will see a circle accompanied by sound. 
-Your task is to report the number of flashes you think you saw after each trial.
-
-There will be {N_TRIALS} trials in total.
-
-Press the spacebar to start.""")
-
-exp.add_data_variable_names(["trial", "nflashes", "nbeeps", "response_flashes"])
 
 #A list of flash and beep combinations, randomized and saved.
 combinations = []
@@ -55,25 +41,48 @@ for f in range(1, MAX_FLASH + 1):
     for b in range(1, MAX_BEEPS + 1):
         combinations.append((f, b))
 
-combinations.append((1,1))
-combinations.append((1,2))
-combinations.append((1,3))
-combinations.append((1,4))
-combinations = combinations * 2
+for b in range(1, 5):
+    combinations.append((1, b))
 
-random.seed(42)
+combinations = combinations * 2
+random.seed(32)
 
 random.shuffle(combinations)
-print(combinations)
+
+#number of trials
+N_TRIALS = len(combinations)
 
 #fixation cross added
+fixation_cross = stimuli.FixCross(size=(24,24), colour=WHITE)
 
-fixation_cross = stimuli.FixCross()
+#visual stimulus
+visual_stimulus = stimuli.Circle(radius=50, colour=WHITE, position=eccentricity_screen_coordinates, )  #stimuli positied according to the eccentricity
 
-# Start the experiment
+blankscreen = stimuli.BlankScreen()
+
+#Instructions for the participants, variables for data file
+
+#exp_fonts=expyriment.misc.list_fonts()
+
+instructions = stimuli.TextScreen("Instructions",
+    f"""You will see a circle accompanied by sound. 
+Your task is to report the number of flashes you saw after each trial.
+
+There will be {N_TRIALS} trials in total.
+
+Press the spacebar to start.""", heading_font="freesans", text_font="freesans", 
+heading_bold=True, heading_size=55, text_size=36, text_italic=True, text_colour=(255,255,255))
+
+exp.add_data_variable_names(["trial", "nflashes", "nbeeps", "response_flashes"])
+
+
+# Start of the experiment
 control.start()
+instructions.present()
+exp.keyboard.wait()
 
-old_tf= exp.clock.time
+#looping through flashes and beeps, 
+#inside the main loop starting with the beeps and adjusting the wait time before the first flash
 for trial, (n_flashes, n_beeps) in enumerate(combinations, start=1):
 
     fixation_cross.present()
@@ -81,20 +90,20 @@ for trial, (n_flashes, n_beeps) in enumerate(combinations, start=1):
     blankscreen.present()
 
     beeps[n_beeps].present()
-    exp.clock.wait(16)  # Wait for 16 ms before the first flash
+    exp.clock.wait(WAIT_AFTER_FIRST_BEEP)  # Waiting for 16 ms before the first flash
+    #loop for flashes 
     for f in range(n_flashes):
         visual_stimulus.present(log_event_tag=True)
-        tf = exp.clock.time
-        print(tf - old_tf)
-        old_tf = tf
         blankscreen.present(log_event_tag=True)
         exp.clock.wait(FLASH_INTERVAL)
 
-    exp.clock.wait(1000)
-    text_input = io.TextInput("How many flashes did you see?")
+    exp.clock.wait(500) #wait after stimuli for half a second
+    #getting participant's answer
+    text_input = io.TextInput("How many flashes did you see?", message_colour=WHITE, user_text_colour= WHITE)
     response = text_input.get()
     exp.data.add([trial, n_flashes, n_beeps, response])
 
 
 # End the experiment
 control.end()
+
